7c13281123b1f20b2809cdb40987fade84d4a66c
Gean634N <gean.a.l.c@gmail.com>
2022-10-23 15:39:25 -0400

    faz funcionar o que será a opção pincel
    a partit desse momento minhas filhas ja começaram a usar a ferramenta

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,28 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+let isDrawing = false;
+let prevMouseX;
+let prevMouseY;
+
 const setBackground = () => {
   ctx.fillStyle = '#fff';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+const draw = {
+  start: (e) => {
+    isDrawing = true;
+    prevMouseX = e.offsetX; // passa a posição atual de X para a variavel prevMauseX
+    prevMouseY = e.offsetY; // passa a posição atual de Y para a variavel prevMauseY
+    ctx.beginPath(); // cria um novo caminho, ou seja, faz com que cada traço seja independente
+  },
+  drawing: (e) => {
+    if(!isDrawing) return;
+    ctx.lineTo(e.offsetX, e.offsetY); // cria linha seguindo o ponteiro do mause
+    ctx.stroke(); // desenha a linha
+  },
+  stop: () => isDrawing = false,
 }
 
 window.addEventListener('load', () => {
@@ -12,3 +31,6 @@ window.addEventListener('load', () => {
   setBackground();
 });
 
+canvas.addEventListener('mousedown', draw.start)
+canvas.addEventListener('mousemove', draw.drawing)
+canvas.addEventListener('mouseup', draw.stop)
